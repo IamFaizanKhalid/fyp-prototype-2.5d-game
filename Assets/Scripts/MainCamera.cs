690b515cdf2416879c86e7f452d2f006a7228124
Faizan Khalid <IamFaizanKhalid@gmail.com>
2020-06-14 17:27:33 +0500

    Initial Version

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // Variables
+    public Transform player;
+    public float smooth;
+    public float height;
+
+    public Vector3 velocity = Vector3.zero;
+
+    int moveX = 15;
+
+    private Vector3 prevPosition = Vector3.zero;
+
+
+
+    // Methods
+
+    void Start()
+    {
+        prevPosition = player.position;
+    }
+
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+        if (prevPosition.x < player.position.x)
+            pos.x = player.position.x + moveX;
+        else if (prevPosition.x > player.position.x)
+            pos.x = player.position.x - moveX;
+        prevPosition = player.position;
+        //pos.y = player.position.y;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+    }
+
+}
---
+++ Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    public float speed = 0.7f;
+    public GameObject target;
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+}
